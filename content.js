c79887bc762b35408cbf985a4e157cdac7e3347a
Tomasz Zając <tomezpl@gmail.com>
2016-03-30 20:56:33 +0100

    Updated for both profile and 'my clips' pages

--- content.js
+++ content.js
@@ -1,7 +1,23 @@
 function getVideo()
 {
 	var vid = document.getElementsByClassName("xm2-mp4")[0];
-	window.location.href = vid.src.replace("http", "https");
+	window.location.href = vid.src.replace("http:", "https:");
 }
 
-window.onload = function(){if(confirm("Do you want to download this video?"))getVideo();};
+var interval = setInterval(pageHasLoaded, 3000);
+
+window.onload = pageHasLoaded;
+
+function pageHasLoaded(){
+	if(document.getElementsByClassName("xm2-mp4")[0].src.indexOf("gameclipscontent-") == -1)
+	{
+		interval = setInterval(3000, pageHasLoaded);
+	}
+	else
+	{
+		if(confirm("Do you want to download this video?"))
+			getVideo();
+		else
+			clearInterval(interval);
+	}
+}
